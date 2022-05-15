3dafdd80a47d7535bb52b35453df9b5ea498b408
innellea <rolandkiraly94@gmail.com>
2022-05-16 00:51:02 +0100

    handle data from api

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,92 +2,105 @@ import { Combobox, Transition } from '@headlessui/react';
 import { SearchIcon, SelectorIcon } from '@heroicons/react/solid';
 import { Fragment, useState } from 'react';
 import useRequest from '../hooks/useRequest';
+import ClickAwayListener from 'react-click-away-listener';
 export const url = 'https://u50g7n0cbj.execute-api.us-east-1.amazonaws.com/v2';
 
 const Search = ({ selectLocation }) => {
-    const [selected, setSelected] = useState([]);
+    const [selected, setSelected] = useState('');
     const [query, setQuery] = useState('');
-
-    /**
-     * @description Provided design seems to limit results to GB location
-     */
+    const [showResults, setShowResults] = useState(false);
+    const cityArray =
+        useRequest(`https://docs.openaq.org/v2/cities?limit=100&page=1&offset=0&sort=asc&country_id=GB&order_by=city
+    `);
     const { data, isLoading, error } = useRequest(
-        `${url}/cities?country=GB${
-            query.length
-                ? `&city=${query.charAt(0).toUpperCase() + query.slice(1)}`
-                : ''
-        }`,
-        {
-            fallback: []
-        }
+        `${url}/cities?country=GB${query.length ? `&city=${query}` : ''}`
     );
-
+    const urlTest = `${url}/cities?country=GB${
+        query.length ? `&city=${query}` : ''
+    }`;
+    console.log(urlTest);
     return (
-        <Combobox value={selected} onChange={setSelected}>
-            <div className='relative mt-1'>
-                <div className='relative z-10 w-full overflow-hidden text-left border-2 border-gray-500 rounded-lg outline-none cursor-default bg-white-50 sm:text-sm'>
-                    <Combobox.Input
-                        className='w-full px-4 py-3 pl-10 text-sm leading-5 border-none rounded-lg outline-none text-black-50'
-                        displayValue={(location) => location.city}
-                        onChange={(event) => setQuery(event.target.value)}
+        <ClickAwayListener onClickAway={() => setShowResults(false)}>
+            <div className='relative'>
+                <div className='relative z-20'>
+                    <input
+                        type='search'
+                        value={selected}
                         placeholder='Enter city name'
                         autoComplete='off'
+                        aria-busy={isLoading}
+                        className='w-full px-4 py-3 pl-12 text-sm leading-5 border-2 border-gray-500 outline-none rounded-xl text-black-50'
+                        onChange={(event) => {
+                            setQuery(event.target.value);
+                            setSelected(event.target.value);
+                        }}
+                        onFocus={() => setShowResults(true)}
                     />
-                    <Combobox.Button className='absolute inset-y-0 flex items-center pr-2 left-3'>
-                        <SearchIcon
-                            className='w-5 h-5 text-gray-500'
-                            aria-hidden='true'
-                        />
-                    </Combobox.Button>
+                    <SearchIcon
+                        className='absolute top-0 h-full text-gray-500 left-3 w-7'
+                        aria-hidden='true'
+                    />
                 </div>
-                <div className='-mt-2 overflow-hidden rounded-md'>
-                    <Transition
-                        as={Fragment}
-                        leave='transition ease-in duration-100'
-                        leaveFrom='opacity-100'
-                        leaveTo='opacity-0'
-                        afterLeave={() => setQuery('')}
-                    >
-                        <Combobox.Options className='relative z-0 w-full py-1 overflow-auto text-base shadow-lg max-h-60 bg-white-50 ring-1 ring-blue ring-opacity-5 focus:outline-none sm:text-sm'>
-                            {data?.results && data.results.length ? (
-                                data.results.map((location) => (
-                                    <Combobox.Option
-                                        key={location.city}
-                                        className={({ active }) =>
-                                            `relative block w-full  cursor-default select-none py-2 px-4 text-left text-black-50 hover:cursor-pointer ${
-                                                active ? 'bg-gray-50' : ''
-                                            }`
-                                        }
-                                        value={location.city}
-                                        as='button'
-                                        onClick={() => selectLocation(location)}
-                                    >
-                                        {({ selected }) => (
-                                            <>
-                                                <span
-                                                    className={`block truncate ${
-                                                        selected
-                                                            ? 'font-medium'
-                                                            : 'font-normal'
-                                                    }`}
-                                                >
-                                                    {location.city}
-                                                </span>
-                                            </>
-                                        )}
-                                    </Combobox.Option>
-                                ))
-                            ) : (
-                                <div className='relative px-4 py-2 cursor-default select-none text-black-50'>
-                                    Nothing found.
-                                </div>
-                            )}
-                        </Combobox.Options>
-                    </Transition>
+                <div>
+                    {showResults && (
+                        <div className='relative z-10 pt-8 pb-4 -mt-4 overflow-hidden rounded-xl bg-white-50'>
+                            <div className='max-h-52 space-y-0.5 overflow-y-auto text-black-50'>
+                                {data.results
+                                    ? cityArray.data.results
+                                          .filter(function (location) {
+                                              if (
+                                                  location.city
+                                                      .toLowerCase()
+                                                      .includes(
+                                                          query.toLowerCase()
+                                                      )
+                                              ) {
+                                                  console.log(
+                                                      `filter:${location.city}`
+                                                  );
+
+                                                  return location.city
+                                                      .toLowerCase()
+                                                      .includes(
+                                                          query.toLowerCase()
+                                                      );
+                                              } else return '';
+                                          })
+                                          .map((location) => (
+                                              <button
+                                                  key={location.city}
+                                                  className='block w-full px-4 py-2 text-left truncate outbuttonne-none hover:bg-gray-50 focus:bg-gray-50'
+                                                  aria-label={location.city}
+                                                  onClick={() => {
+                                                      selectLocation(location);
+                                                      setShowResults(false);
+                                                      setSelected('');
+                                                  }}
+                                                  onMouseEnter={() =>
+                                                      setSelected(location.city)
+                                                  }
+                                                  onMouseLeave={() =>
+                                                      setSelected('')
+                                                  }
+                                              >
+                                                  {location.city}
+                                              </button>
+                                          ))
+                                    : ''}
+
+                                {!isLoading && !data?.results && (
+                                    <p className='relative px-4 py-2 cursor-default select-none'>
+                                        Nothing found.
+                                    </p>
+                                )}
+
+                                {/** No loading state specified */}
+                            </div>
+                        </div>
+                    )}
                 </div>
             </div>
-        </Combobox>
+        </ClickAwayListener>
     );
 };
-
 export default Search;
